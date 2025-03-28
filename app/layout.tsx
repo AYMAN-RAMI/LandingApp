4b8947ff0dea90227749df83ca40b01b7db447fb
AYMAN RAMI <06ayman.rami@gmail.com>
2025-03-28 22:52:00 +0000

    Ton message de commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '$KNOWLEDGE ',
+  title: 'MR $KNOWLEDGE ',
   description: 'Created with v0',
   generator: 'v0.dev',
   icons: {
